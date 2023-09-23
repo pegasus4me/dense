46b7984cd46b689f20f6ad98c5a868bbfbcc1628
pegasus4me <safoantouil@outlook.fr>
2023-09-23 12:59:31 +0200

    CONTRACTS DEPLOYED TO ARB GOERLI(crowdFunding.sol)

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,9 +1,16 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const Dense = await ethers.deployContract("CrowdFunding");
+  
+  const Auth = await ethers.deployContract("Authentification");
+  await Auth.waitForDeployment()
+// ---------------------------------------------
+  const Vault = await ethers.deployContract("Vault");
+  await Vault.waitForDeployment()
+//-----------------------------------------------
+  const Dense = await ethers.deployContract("CrowdFunding", [await Auth.getAddress(), await Vault.getAddress()]);
   await Dense.waitForDeployment();
-  const DenseContract  = await Dense.getAddress()
+  console.log(`contract succesfully deployed in this contract =>${await Dense.getAddress()}`)
 } 
 main().catch((error) => {
   console.error(error);
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv"
+dotenv.config()
 
 const config: HardhatUserConfig = {
+  
   solidity: "0.8.19",
+  networks : {
+    
+    arbitrumGoerli: {
+      url: process.env.PROVIDER_KEY_URL as string | undefined,
+      chainId: 421613,
+      accounts: [process.env.PRIVATE_KEY as string]
+    },
+  
+  },
+  etherscan :  {
+    apiKey : process.env.APYV2AYS7UMRVW3XAF9NK5XM72DPIQSAUV
+  }
 };
 
 export default config;
